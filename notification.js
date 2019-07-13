c7d394339b9342b44d5f79066774a5aeff8b0525
Akmal Hazim <akmalhazim@inspyrehosting.com>
2019-07-13 13:24:27 +0800

    fixed some bugs on server.js (for testing)

--- notification.js
+++ notification.js
@@ -9,13 +9,17 @@ class Notification {
 	}
 
 	notify(type, driver, recipient, json, options = {}) {
-		return new Promise((resolve, reject) => {
+		return new Promise(async (resolve, reject) => {
 			if(!this.queue) {
 				var queue = options.queue
 			} else {
 				var queue = this.queue
 			}
 			await this.ch.assertQueue(queue)
+			const timer = 5000
+			console.log(`Starting timer: ${timer}`)
+			await delay(timer)
+			console.log('Finished timer.')
 			this.ch.sendToQueue(queue, Buffer.from(JSON.stringify(json)), {
 				contentType: 'application/json',
 				headers: {
@@ -29,6 +33,12 @@ class Notification {
 		})
 		
 	}
+}
+
+function delay(ms) {
+	return new Promise(async function(resolve, reject) {
+		setTimeout(() => resolve(), ms);
+	})
 }
 
 module.exports = Notification;
--- server.js
+++ server.js
@@ -1,35 +1,19 @@
-const notification = require('./notification');
+const _notification = require('./notification');
 
-// amqp setup
-require('amqplib/callback_api')
-  .connect('amqp://localhost', function(err, conn) {
-   	const ch = conn.createChannel(function(err, ch) {
-   		const noti = new notification(ch, 'notifications')
-   		setInterval(function() {
-   			noti.notify('NEW_DATA', 'telegram', 'YOUR_CHAT_ID', {
-   				humidity: Math.random(3,10),
-   				temp: Math.random(100,200)
-   			})
-   				.then(res => {
-   					// 
-   				})
-   				.catch(err => {
-   					console.error(err)
-   				})
-   		}, 500)
 
-   		ch.consume('notifications', function(msg) {
-   			console.log(msg.content.toString())
-   			ch.ack(msg)
-   		})
-   	})
-    
-  });
-
+tryAmqp();
 
  async function tryAmqp() {
  	const conn = await require('amqplib').connect('amqp://localhost');
  	const ch = await conn.createChannel();
-
-
+ 	const notification = new _notification(ch)
+ 	try {
+	 	await notification.notify('NEW_DATA', 'telegram', 'YOUR_CHAT_ID', {
+	 		content: 'somecontent',
+	 		timestamp: new Date
+	 	})
+ 	} catch(err) {
+ 		console.error(err)
+ 		process.exit(1)
+ 	}
  }
